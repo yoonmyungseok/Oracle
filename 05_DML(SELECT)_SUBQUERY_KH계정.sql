31b3e5d3efebe65f347a2ca5c4af01efad96e1d8
yoonmyungseok <yoonms5319@gmail.com>
2022-03-24 18:08:36 +0900

    0324 ORACLE SUBQUERY

--- 05_DML(SELECT)_SUBQUERY_KH계정.sql
+++ 05_DML(SELECT)_SUBQUERY_KH계정.sql
@@ -376,6 +376,7 @@ FROM EMPLOYEE
 WHERE (NVL(DEPT_CODE,'없음'), SALARY) IN (SELECT NVL(DEPT_CODE,'없음'), MAX(SALARY) FROM EMPLOYEE GROUP BY DEPT_CODE)
 ORDER BY SALARY DESC;
 
+
 -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
@@ -387,3 +388,81 @@ ORDER BY SALARY DESC;
     
     서브쿼리를 수행한 결과 (ResultSet)을 테이블 대신에 사용함
 */
+--보너스 포함 연봉이 3000만원 이상인 사원들의 사번, 이름, 보너스를 포함한 연봉, 부서코드를 조회
+--1) 인라인뷰를 안 쓴 버전(테이블로부터 조회하겠다)
+SELECT EMP_ID "사번", EMP_NAME "사원명", (SALARY+(SALARY*NVL(BONUS,0)))*12 "보너스 포함 연봉", DEPT_CODE "부서코드"
+FROM EMPLOYEE
+WHERE (SALARY+(SALARY*NVL(BONUS,0)))*12 >=30000000;
+--2)인라인뷰를 쓴 버전(리절트셋으로부터 조회하겠다)
+SELECT "사번", "사원명", "보너스 포함 연봉", "부서코드"
+FROM (SELECT EMP_ID "사번", EMP_NAME "사원명", (SALARY+(SALARY*NVL(BONUS,0)))*12 "보너스 포함 연봉", DEPT_CODE "부서코드" FROM EMPLOYEE)
+WHERE "보너스 포함 연봉" >=30000000;
+
+-->>인라인 뷰를 주로 사용하는 예
+--TOP-N 분석 : 데이터베이스에 있는 자료 중 최상위 몇 개의 자료를 보기 위해 사용하는 분석 기법
+
+--전 직원 중 급여가 가장 높은 상위 5명의 이름, 급여를 조회
+-- ROWNUM : 오라클에서 제공하는 컬럼, 조회된 순서대로 1부터 순번을 부여해주는 컬럼
+SELECT ROWNUM, EMP_NAME, SALARY
+FROM EMPLOYEE
+WHERE ROWNUM<=5 --5등안에 든다==번호표숫자가 5이하다
+ORDER BY SALARY DESC;
+-->순서가 예상과 다르게 뒤죽박죽 나온다
+
+--문제 원인: TOP-N 분석을 하려면 일단 정렬을 하고 그 다음에 번호표를 부여해줘야하는데 SELECT문의 실행 순서 상 그 순서가 바껴있기 때문
+--해결 방법: 정렬이 먼저 일어나게하고 번호표를 부여해야한다
+SELECT ROWNUM, EMP_NAME, SALARY --실행순서4
+FROM --실행순서2
+(SELECT * FROM EMPLOYEE ORDER BY SALARY DESC) --실행순서1 (상대적으로 ORDER BY절이 SELECT 절보다 먼저 실행되게끔 해주는 효과)
+WHERE ROWNUM<=5; --실행순서3
+
+--FROM 절의 인라인뷰에 별칭 또한 부여 가능함
+--이 때, 메인쿼리의 SELECT절에 별칭.* 을 작성하면 해당 인라인뷰의 모든 컬럼을 가져올 수 있다
+SELECT ROWNUM, E.*
+FROM(SELECT * FROM EMPLOYEE ORDER BY SALARY DESC) E
+WHERE ROWNUM<=5;
+
+--FROM 절의 인라인뷰에 그룹함수식이 포함된다면 항상 별칭을 붙여줘야 한다
+--각 부서별 평균 급여가 높은 3개의 부서의 부서코드, 평균 급여 조회
+SELECT ROWNUM, DEPT_CODE, "평균 급여"
+FROM(SELECT DEPT_CODE, AVG(SALARY) "평균 급여" FROM EMPLOYEE GROUP BY DEPT_CODE ORDER BY AVG(SALARY) DESC) 
+WHERE ROWNUM<=3;
+
+--가장 최근에 입사한 사원 5명을 조회(사원명, 급여, 입사일)
+SELECT ROWNUM, E.*
+FROM(SELECT EMP_NAME, SALARY, HIRE_DATE FROM EMPLOYEE ORDER BY HIRE_DATE DESC) E
+WHERE ROWNUM<=5;
+
+--TOP-N 분석 주의사항: 항상 정렬 (ORDER BY) 후에 순번매기기 (ROWNUM)을 해야한다
+--ORDER BY절은 항상 실행순서가 마지막이라 먼저 실행하려면 인라인뷰에 작성해줘야 한다라는 것
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+    6. 순위 매기는 함수
+
+    -RANK() OVER(정렬기준) : 공동 1위가 3명이라고 한다면 그 다음 순위를 4위로 하겠다
+    -DENSE_RANK() OVER(정렬기준) : 공동 1위가 3명이여도 그 다음 순위를 무조건 2위로 하겠다
+    
+    주의사항: SELECT 절에만 작성 가능
+*/
+
+--사원들의 급여가 높은 순서대로 사원명, 급여, 순위 조회
+
+--순위매기는 함수 안 쓴 버전
+SELECT EMP_NAME, SALARY, ROWNUM "순위"
+FROM(SELECT EMP_NAME, SALARY FROM EMPLOYEE ORDER BY SALARY DESC);
+
+--순위매기는 함수 사용한 버전
+--1) RANK() OVER(정렬기준) 함수 사용
+SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) "순위"
+FROM EMPLOYEE;
+
+--2)DENSE_RANK() OVER(정렬기준) 함수 사용
+SELECT EMP_NAME, SALARY, DENSE_RANK() OVER(ORDER BY SALARY DESC) "순위"
+FROM EMPLOYEE;
+
+--5위까지만 조회하겠다
+SELECT *
+FROM (SELECT EMP_NAME, SALARY, RANK() OVER(ORDER BY SALARY DESC) "순위" FROM EMPLOYEE) 
+WHERE "순위" <=5;
